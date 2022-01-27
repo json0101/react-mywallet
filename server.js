ad8d8db7fa13b02da588646e485c4b1b211871b7
json0101 <jasonhernandezaguilar@gmail.com>
2022-01-27 15:59:05 -0600

    package config

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001
 
 app.use(express.static('build'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
       }
