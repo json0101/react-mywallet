4e5e06cfaef1a5de2ddacd8a26c72b0ceba39c0f
json0101 <jasonhernandezaguilar@gmail.com>
2022-01-27 15:51:21 -0600

    package config

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.use(express.static('build'))
+app.use(express.static('build'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 app.listen(PORT, console.log("Listening on port" + PORT));
 
 // //"start": "react-scripts start",
